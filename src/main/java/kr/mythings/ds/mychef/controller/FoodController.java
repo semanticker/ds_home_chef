ba261700692439ece8f17af983fe7c48c84ab981
semanticker <semanticker@gmail.com>
2022-08-27 13:45:57 +0900

    food controller skeleton

--- src/main/java/kr/mythings/ds/mychef/controller/FoodController.java
+++ src/main/java/kr/mythings/ds/mychef/controller/FoodController.java
@@ -3,7 +3,9 @@ package kr.mythings.ds.mychef.controller;
 import kr.mythings.ds.mychef.service.FoodService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,6 +21,26 @@ public class FoodController {
         foodService.list();
 
         return "food/foodList";
+    }
+
+    @GetMapping("/add")
+    public String add(){
+        return "";
+    }
+
+    @PutMapping("/add")
+    public String save() {
+        return "";
+    }
+
+    @GetMapping("/{id}")
+    public String detail() {
+        return "";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete() {
+        return "";
     }
 
 }
