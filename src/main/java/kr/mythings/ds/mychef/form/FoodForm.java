87e3f9fb3088f3289247a67677cc1b6eb5efbe3f
semanticker <semanticker@gmail.com>
2022-12-03 00:40:03 +0900

    Serving Controller 스켈레톤 작성

--- src/main/java/kr/mythings/ds/mychef/form/FoodForm.java
+++ src/main/java/kr/mythings/ds/mychef/form/FoodForm.java
@@ -4,8 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter @Setter
 public class FoodForm {
--- src/main/java/kr/mythings/ds/mychef/form/ServingForm.java
+++ src/main/java/kr/mythings/ds/mychef/form/ServingForm.java
@@ -3,10 +3,25 @@ package kr.mythings.ds.mychef.form;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 @Getter
 @Setter
 public class ServingForm {
 
     private Long id;
+
+    private String foodId;
+
+    private String foodName;
+
+    private String recipeId;
+
+    private String recipeName;
+
+    private String recipeFrom;
+
+    private ArrayList<CustomerDTO> list;
+
 
 }
