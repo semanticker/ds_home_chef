0be0f6818b9b0080146f629b1e279dde488979c0
semanticker <semanticker@gmail.com>
2023-01-12 23:35:43 +0900

    이미지 갤러리 blueimp-gallery 검토   이미지 lightbox 테스트3

--- src/main/java/kr/mythings/ds/mychef/controller/FileController.java
+++ src/main/java/kr/mythings/ds/mychef/controller/FileController.java
@@ -4,9 +4,11 @@ import kr.mythings.ds.mychef.common.SuccessResult;
 import kr.mythings.ds.mychef.domain.FileEntity;
 import kr.mythings.ds.mychef.service.FileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,9 +17,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.UriUtils;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
+import java.util.BitSet;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,7 +31,7 @@ public class FileController {
     private final FileService fileService;
 
     //   이미지 출력
-    @GetMapping("/images/{fileId}")
+    @GetMapping("/images2/{fileId}")
     @ResponseBody
     public Resource downloadImage(@PathVariable("fileId") Long id, Model model) throws IOException {
 
@@ -38,6 +43,28 @@ public class FileController {
             return null;
         }
     }
+
+    @GetMapping("/images/{fileId}")
+    @ResponseBody
+    public ResponseEntity<InputStreamResource>  downloadImage2(@PathVariable("fileId") Long id, Model model) throws IOException {
+
+        FileEntity file = fileService.findOne(id);
+
+        if (file != null) {
+            //return new UrlResource("file:" + file.getFileSaveName());
+
+            UrlResource urlResource = new UrlResource("file:" + file.getFileSaveName());
+
+            return ResponseEntity.ok()
+                    .contentLength(file.getFileSize())
+                    .contentType(MediaType.parseMediaType(file.getFileExtName()))
+                    .body(new InputStreamResource(urlResource.getInputStream()));
+        } else {
+            return null;
+        }
+    }
+
+
 
     //
     @GetMapping("/attach/download/{fileId}")
@@ -51,7 +78,6 @@ public class FileController {
 
         String contentDisposition = "attachment; filename=\"" + encode + "\"";
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition).body(resource);
-
     }
 
     @GetMapping(value = "/attach/delete/{fileId}" , produces = "application/json; charset=UTF-8")
